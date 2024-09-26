1cb3c172a3864c6f243f01eab3ce8596c4f82cbf
LIZANDRO EDGAR QUICHE TRUJILLO <Avanza-lizandro.quiche@spsa.pe>
2024-09-25 23:51:36 -0500

    se agrega la card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -90,13 +90,13 @@ export default function Home() {
   const platos = [
     {
       id: 1,
-      nombre: 'Pollo a la Brasa',
+      nombre: ' 1 Pollo a la Brasa',
       precio: 'S/ 45.00',
       imagen: '/images/PolloEntero.png',
     },
     {
       id: 2,
-      nombre: 'Salchipapas',
+      nombre: '1/8 Pollo a la Brasa',
       precio: 'S/ 12.00',
       imagen: '/images/OctavoPollo.png',
     }
@@ -184,7 +184,7 @@ export default function Home() {
       {/* Sección de platos */}
       <section className="py-10 bg-gray-100">
         <div className="container mx-auto">
-          <h2 className="text-3xl font-bold mb-6 text-center">Nuestro Menú</h2>
+          <h2 className="text-3xl font-bold mb-6 text-center text-gray-700 ">Nuestro Menú</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {platos.map(plato => (
               <div key={plato.id} className="bg-white p-6 rounded-lg shadow-md">
@@ -197,7 +197,7 @@ export default function Home() {
                     className="rounded-lg"
                   />
                 </div>
-                <h3 className="text-xl font-bold mb-2">{plato.nombre}</h3>
+                <h3 className="text-xl font-bold mb-2 text-gray-700">{plato.nombre}</h3>
                 <p className="text-lg font-semibold text-gray-700">{plato.precio}</p>
               </div>
             ))}
