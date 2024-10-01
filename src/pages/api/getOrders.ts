856ed8ee8aed9bbc77613333aaef8faaba60a7ba
LIZANDRO EDGAR QUICHE TRUJILLO <Avanza-lizandro.quiche@spsa.pe>
2024-09-30 21:03:02 -0500

    VISTA DE ORDEN

---
+++ src/pages/api/getOrders.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    try {
+      const orders = await prisma.pedido.findMany({
+        include: {
+          items: {
+            include: {
+              Product: true, // Incluir detalles del producto
+            },
+          },
+        },
+      });
+
+      res.status(200).json(orders);
+    } catch (error) {
+      console.error("Error al obtener los pedidos:", error);
+      res.status(500).json({ message: 'Error al obtener los pedidos', error });
+    }
+  } else {
+    res.status(405).json({ message: 'Método no permitido' });
+  }
+}
