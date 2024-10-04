9181c1603ccf51a70a57d99afdcd10a1ef5573e9
LIZANDRO EDGAR QUICHE TRUJILLO <Avanza-lizandro.quiche@spsa.pe>
2024-10-04 18:40:54 -0500

    filtro de familia para busqueda

--- src/app/Carta/page.tsx
+++ src/app/Carta/page.tsx
@@ -4,12 +4,14 @@ import Image from "next/image";
 import { useCart } from "@/context/CartContext";
 import Header from "@/components/Header";
 import { useRouter } from "next/navigation";
+
 interface Product {
   id: number;
   name: string;
   price: number;
   imagenUrl: string;
   branchId: number;
+  familia: string; // Asegúrate de incluir familia en los productos
 }
 
 interface Branch {
@@ -22,13 +24,25 @@ interface User {
   email: string;
 }
 
+const familias = [
+  "Pollos a la brasa",
+  "Chifa",
+  "Platos a la carta",
+  "Parrillas",
+  "Guarniciones",
+  "Bebidas sin alcohol",
+  "Bebidas con alcohol",
+].map(familia => familia.toUpperCase());
+
 export default function CartaPage() {
   const [products, setProducts] = useState<Product[]>([]);
   const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
   const [branches, setBranches] = useState<Branch[]>([]);
   const [selectedBranch, setSelectedBranch] = useState<number | "">("");
+  const [selectedFamilia, setSelectedFamilia] = useState<string>(""); // Nueva variable para familia
   const [searchTerm, setSearchTerm] = useState("");
   const [user, setUser] = useState<User | null>(null);
+  const [errorMessage, setErrorMessage] = useState(""); // Para manejar el mensaje de error
   const { addToCart, cartItems } = useCart();
   const router = useRouter();
   console.log(products);
@@ -60,44 +74,57 @@ export default function CartaPage() {
       } catch (error) {
         console.error("Error al parsear el usuario del localStorage:", error);
       }
-    }else{
+    } else {
       router.push("/login");
     }
   }, []);
 
-  // Función para cargar productos por sucursal
+  // Función para cargar productos por sucursal y familia
   const fetchProducts = useCallback(
-    async (branchId: number | "") => {
+    async (branchId: number | "", familia: string) => {
       try {
         const res = await fetch("/api/getProducts", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ branchId }),
+          body: JSON.stringify({ branchId, familia }), // Enviar familia al backend
         });
-        if (res.ok) {
-          const data: Product[] = await res.json();
+
+        const data = await res.json();
+
+        if (!res.ok || data.message) {
+          // Si la respuesta no es correcta o contiene un mensaje, limpiar la vista y mostrar error
+          setProducts([]);
+          setFilteredProducts([]);
+          setErrorMessage(data.message || "No se encontraron productos.");
+        } else {
           setProducts(data);
-          filterProducts(data, searchTerm, branchId); // Aplicar el filtro cuando se obtienen los productos
+          filterProducts(data, searchTerm, branchId, familia); // Aplicar el filtro cuando se obtienen los productos
+          setErrorMessage(""); // Limpiar el mensaje de error si se encuentran productos
         }
       } catch (error) {
         console.error("Error al cargar los productos:", error);
+        setErrorMessage("Ocurrió un error al cargar los productos.");
+        setProducts([]);
+        setFilteredProducts([]);
       }
     },
     [searchTerm]
   );
 
-  // Función para filtrar los productos según el término de búsqueda y la sucursal seleccionada
+  // Función para filtrar los productos según el término de búsqueda, la sucursal y la familia seleccionada
   const filterProducts = (
     productList: Product[],
     searchTerm: string,
-    branchId: number | ""
+    branchId: number | "",
+    familia: string
   ) => {
     const filtered = productList.filter((product) => {
       const matchesBranch = branchId === "" || product.branchId === branchId;
+      const matchesFamilia = familia === "" || product.familia === familia;
       const matchesSearch = product.name.toLowerCase().includes(searchTerm);
-      return matchesBranch && matchesSearch;
+      return matchesBranch && matchesFamilia && matchesSearch;
     });
     setFilteredProducts(filtered);
   };
@@ -111,10 +138,15 @@ export default function CartaPage() {
     setSelectedBranch(branchId);
   };
 
+  const handleFamiliaChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedFamilia(e.target.value);
+  };
+
   // Función para ejecutar la búsqueda manual cuando se da clic en el botón "Buscar"
   const handleSearchClick = () => {
-    fetchProducts(selectedBranch); // Llamamos a la API para obtener productos basados en la sucursal seleccionada
+    fetchProducts(selectedBranch, selectedFamilia); // Llamamos a la API para obtener productos basados en la sucursal y la familia seleccionada
   };
+
   const handleLogout = () => {
     localStorage.removeItem("user"); // Limpiar datos del usuario
     setUser(null); // Limpiar el estado del usuario
@@ -130,14 +162,14 @@ export default function CartaPage() {
           value={searchTerm}
           onChange={handleSearch}
           placeholder="Buscar producto..."
-          className="w-3/5 p-3 border rounded-lg focus:outline-none focus:border-yellow-500 text-gray-700"
+          className="w-3/6 p-3 border rounded-lg focus:outline-none focus:border-yellow-500 text-gray-700"
         />
 
         {/* Selector de sucursales */}
         <select
           value={selectedBranch}
           onChange={handleBranchChange}
-          className="w-1/5 p-3 border rounded-lg focus:outline-none focus:border-yellow-500 text-gray-700"
+          className="w-1/6 p-3 border rounded-lg focus:outline-none focus:border-yellow-500 text-gray-700"
         >
           {branches.map((branch) => (
             <option key={branch.id} value={branch.id}>
@@ -146,10 +178,24 @@ export default function CartaPage() {
           ))}
         </select>
 
+        {/* Selector de familia */}
+        <select
+          value={selectedFamilia}
+          onChange={handleFamiliaChange}
+          className="w-1/6 p-3 border rounded-lg focus:outline-none focus:border-yellow-500 text-gray-700"
+        >
+          <option value="">Todas las familias</option>
+          {familias.map((familia) => (
+            <option key={familia} value={familia}>
+              {familia}
+            </option>
+          ))}
+        </select>
+
         {/* Botón Buscar */}
         <button
           onClick={handleSearchClick}
-          className="w-1/5 bg-yellow-500 hover:bg-yellow-600 text-gray-700 font-bold py-2 px-4 rounded-lg"
+          className="w-1/6 bg-yellow-500 hover:bg-yellow-600 text-gray-700 font-bold py-2 px-4 rounded-lg"
         >
           Buscar
         </button>
@@ -157,47 +203,51 @@ export default function CartaPage() {
 
       <main className="flex-grow">
         <section className="container mx-auto p-4">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {filteredProducts.length > 0 ? (
-              filteredProducts.map((product) => (
-                <div
-                  key={product.id}
-                  className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center transition-all hover:shadow-xl transform hover:scale-105"
-                >
-                  <div className="relative w-full h-48 sm:h-56 lg:h-64 mb-4">
-                    <Image
-                      src={product.imagenUrl || "/images/default.png"}
-                      alt={product.name}
-                      layout="fill"
-                      objectFit="cover"
-                      className="rounded-lg"
-                    />
-                  </div>
-                  <h3 className="text-xl font-bold text-center text-gray-800">
-                    {product.name}
-                  </h3>
-                  <p className="text-lg font-semibold text-center text-gray-700">
-                    S/ {product.price.toFixed(2)}
-                  </p>
-                  <button
-                    onClick={() =>
-                      addToCart({
-                        ...product,
-                        quantity: 1, // Añadir la cantidad por defecto cuando se añade al carrito
-                      })
-                    }
-                    className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-lg mt-4"
+          {errorMessage ? (
+            <p className="text-center col-span-4 text-gray-700">{errorMessage}</p>
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+              {filteredProducts.length > 0 ? (
+                filteredProducts.map((product) => (
+                  <div
+                    key={product.id}
+                    className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center transition-all hover:shadow-xl transform hover:scale-105"
                   >
-                    Añadir al carrito
-                  </button>
-                </div>
-              ))
-            ) : (
-              <p className="text-center col-span-4 text-gray-700">
-                No se encontraron productos.
-              </p>
-            )}
-          </div>
+                    <div className="relative w-full h-48 sm:h-56 lg:h-64 mb-4">
+                      <Image
+                        src={product.imagenUrl || "/images/default.png"}
+                        alt={product.name}
+                        layout="fill"
+                        objectFit="cover"
+                        className="rounded-lg"
+                      />
+                    </div>
+                    <h3 className="text-xl font-bold text-center text-gray-800">
+                      {product.name}
+                    </h3>
+                    <p className="text-lg font-semibold text-center text-gray-700">
+                      S/ {product.price.toFixed(2)}
+                    </p>
+                    <button
+                      onClick={() =>
+                        addToCart({
+                          ...product,
+                          quantity: 1, // Añadir la cantidad por defecto cuando se añade al carrito
+                        })
+                      }
+                      className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-lg mt-4"
+                    >
+                      Añadir al carrito
+                    </button>
+                  </div>
+                ))
+              ) : (
+                <p className="text-center col-span-4 text-gray-700">
+                  No se encontraron productos.
+                </p>
+              )}
+            </div>
+          )}
         </section>
       </main>
     </div>
