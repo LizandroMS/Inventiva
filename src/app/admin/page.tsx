f018323251f596bb7cc46c93cc23ea02b190278d
LIZANDRO EDGAR QUICHE TRUJILLO <Avanza-lizandro.quiche@spsa.pe>
2024-10-13 14:59:27 -0500

    mejora de vusta del administrador

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -52,13 +52,13 @@ export default function AdminPage() {
   return (
     <div>
       <Header />
-      <div className="container mx-auto py-10 px-4 md:px-8 lg:px-16">
-        <h1 className="text-3xl font-bold mb-8 text-center text-white-800">
+      <div className="container mx-auto py-10 px-4 md:px-8 lg:px-16 bg-gradient-to-br from-gray-50 via-blue-100 to-indigo-100 min-h-screen">
+        <h1 className="text-4xl font-bold mb-8 text-center text-gray-900">
           Panel de Administración
         </h1>
 
         {/* Grid para las tarjetas */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {/* Card: Registro de Personal */}
           <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
             <div className="flex items-center justify-center mb-4 text-yellow-500">
