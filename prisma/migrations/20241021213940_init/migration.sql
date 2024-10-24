af3526efcc988d9de03ea52ee191941ac6cfe34c
LIZANDRO EDGAR QUICHE TRUJILLO <Avanza-lizandro.quiche@spsa.pe>
2024-10-24 15:05:42 -0500

    bd

--- prisma/migrations/20241021213940_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `pedido` ADD COLUMN `companyAddress` VARCHAR(191) NULL,
-    ADD COLUMN `companyName` VARCHAR(191) NULL,
-    ADD COLUMN `paymentType` VARCHAR(191) NULL DEFAULT 'Boleta',
-    ADD COLUMN `ruc` VARCHAR(191) NULL;
--- prisma/migrations/20241024200532_init/migration.sql
+++ prisma/migrations/20241024200532_init/migration.sql
@@ -63,6 +63,10 @@ CREATE TABLE `Pedido` (
     `totalAmount` DOUBLE NOT NULL,
     `status` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `paymentType` VARCHAR(191) NULL DEFAULT 'Boleta',
+    `ruc` VARCHAR(191) NULL,
+    `companyName` VARCHAR(191) NULL,
+    `companyAddress` VARCHAR(191) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
