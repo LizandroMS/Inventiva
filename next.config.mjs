ea6a6f79102bd29a7a500ed45f945a312ffd1a99
LIZANDRO EDGAR QUICHE TRUJILLO <Avanza-lizandro.quiche@spsa.pe>
2024-11-09 15:03:35 -0500

    Cambios

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,18 @@ const nextConfig = {
   reactStrictMode: true,  // Activar modo estricto de React
   swcMinify: true,        // Minificación de código usando SWC
   images: {
-    domains: ['firebasestorage.googleapis.com'],
+      domains: ['firebasestorage.googleapis.com'],
   },
   trailingSlash: false,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        pathname: '/v0/b/**', // Permite todas las rutas dentro de Firebase Storage
-      },
-    ],
-  },
-  productionBrowserSourceMaps: false,
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'firebasestorage.googleapis.com',
+          pathname: '/v0/b/**', // Permite todas las rutas dentro de Firebase Storage
+        },
+      ],
+    },
 };
 
 export default nextConfig;
